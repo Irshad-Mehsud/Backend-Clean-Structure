9ff85946b163af8a73974204041fb7612533a650
Irshad-Mehsud <irshadahmad19059@gmail.com>
2025-09-13 23:07:27 +0500

    Understanding uploading files to Cloudinary

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRoutes from "../modules/user/routes.js";
 import productRoutes from "../modules/products/routes.js";
+import uploadRoutes from "../modules/uploads/routes.js";
 const router = Router();
 
 router.use("/users",userRoutes);
 router.use("/products",productRoutes);
+router.use("/uploads" ,uploadRoutes);
 
 
 
