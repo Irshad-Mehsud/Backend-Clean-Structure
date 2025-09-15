4ae68b33e54572395098a20820f1cb984a9334c7
Irshad-Mehsud <irshadahmad19059@gmail.com>
2025-09-15 23:39:23 +0500

    Adding JWT Authentication

--- src/modules/user/controllers/post.js
+++ src/modules/user/controllers/post.js
@@ -1,13 +1,35 @@
-import { INTERNAL_SERVER_ERROR, POST_DATA_MESSAGE } from "../constants/index.js";
+import {
+  INTERNAL_SERVER_ERROR,
+  POST_DATA_MESSAGE,
+} from "../constants/index.js";
 import postData from "../services/post.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
-const postController =async (req, res) => {
-try {
-    const data= await postData(req.body);
-    res.status(200).json({ status:200, data ,message:POST_DATA_MESSAGE});
-} catch (error) {
-    res.status(500).json({ status:500, error: INTERNAL_SERVER_ERROR });
-}
+const postController = async (req, res) => {
+  try {
+    const data = await postData(req.body);
+    const { email } = req.body;
+
+    if (!process.env.JWT_SECRET) {
+      throw new Error('Missing SECRET_KEY environment variable');
+    }
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET);
+
+    res.status(200).json({
+      status: 200,
+      data,
+      message: POST_DATA_MESSAGE,
+      token,
+    });
+  } catch (error) {
+    console.error('Error in postController:', error); // <-- log actual error
+    res.status(500).json({ status: 500, error: INTERNAL_SERVER_ERROR });
+  }
 };
+
+
 
 export default postController;
--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -3,9 +3,10 @@ import  getController  from "./controllers/get.js";
 import postController from "./controllers/post.js";
 import deleteController from "./controllers/delete.js";
 import updateController from "./controllers/update.js";
+import { authentication } from "../../helpers/index.js";
 const router = Router();
 
-router.get("/", getController);
+router.get("/", authentication , getController);
 router.post("/",postController);
 router.delete("/:id",deleteController);
 router.put("/:id",updateController);
